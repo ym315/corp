2d97f0c432e5c33a1c78aa7b7334134a7d711645
ym315 <ym021315@gamil.com>
2025-08-20 11:37:43 +0900

    upload

---
+++ js/menu.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    //start    
+        $('header').load('menu.html header>div');
+        $('.main-menu').load('menu.html .main-menu-import');
+
+        $('footer').load('menu.html footer>div');
+    
+    
+});
+
